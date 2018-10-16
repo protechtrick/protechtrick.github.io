8de887b92231a3712e376fce72f9292dfd0552be
Tiền Minh Vy <tienminhvydev@gmail.com>
2018-10-16 09:45:57 +0700

    Update

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Pro Tech Trick &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://protechtrick.github.io/blog/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='https://protechtrick.github.io/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -17,7 +17,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="https://protechtrick.github.io/blog/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://protechtrick.github.io/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -29,13 +29,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="https://protechtrick.github.io/blog/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://protechtrick.github.io/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="https://protechtrick.github.io/blog/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="https://protechtrick.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -52,8 +52,8 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="https://protechtrick.github.io/blog/">&larr; Back to Pro Tech Trick</a></p>
-		
+	<p id="backtoblog"><a href="https://protechtrick.github.io/">&larr; Back to Pro Tech Trick</a></p>
+	<div class="privacy-policy-page-link"><a class="privacy-policy-link" href="https://protechtrick.github.io/chinh-sach-bao-mat/">Chính sách bảo mật</a></div>	
 	</div>
 
 	
